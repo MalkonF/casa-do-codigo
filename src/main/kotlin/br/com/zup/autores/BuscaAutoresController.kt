90eee676ce388c2657fa7588b03d676e5021e864
MalkonF <sudo777@protonmail.com>
2021-07-05 03:53:02 -0300

    Mapeando queries explicitamente no Micronaut Data

--- src/main/kotlin/br/com/zup/autores/BuscaAutoresController.kt
+++ src/main/kotlin/br/com/zup/autores/BuscaAutoresController.kt
@@ -19,7 +19,8 @@ class BuscaAutoresController(val autorRepository: AutorRepository) {
             return HttpResponse.ok(resposta)
         }
 
-        val possivelAutor = autorRepository.findByEmail(email)
+        //val possivelAutor = autorRepository.findByEmail(email)
+        val possivelAutor = autorRepository.buscaPorEmail(email)
         if (possivelAutor.isEmpty) {
             return HttpResponse.notFound()
         }
